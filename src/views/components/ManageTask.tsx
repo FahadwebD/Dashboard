5d4caa553faddc50dadcdd8af9fbac80e66afe1f
FahadwebD <fahadchowdhury66779@gmail.com>
2022-06-08 17:24:30 +0600

    i need more and more practise on typescript and redux

--- src/views/components/ManageTask.tsx
+++ src/views/components/ManageTask.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import {
+
+  getTodosSelector,
+
+} from "../../redux/selector";
+import { fetchTodoRequest } from "../../redux/actions";
+
+
+import '../../style/style.css'
 
 
 export const ManageTask = () => {
@@ -9,79 +19,37 @@ export const ManageTask = () => {
     const pen = <FontAwesomeIcon icon={faPen} />
     const remove = <FontAwesomeIcon icon={faTrash} />
 
+    const dispatch = useDispatch();
+    const todos = useSelector(getTodosSelector);
     
+  
+    useEffect(() => {
+      dispatch(fetchTodoRequest());
+    }, []);
+  
   return (
     <div>
          <div>
         <h1>Manage Task</h1>
 
-<table id="customers">
+        <table id="customers">
   <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-    <th>Action</th>
+    <th>Userid</th>
+    <th>Id</th>
+    <th>Task</th>
+    <th>Status</th>
+    
   </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><Link to='/updateTasks' style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</Link><button  style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Berglunds snabbköp</td>
-    <td>Christina Berglund</td>
-    <td>Sweden</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Königlich Essen</td>
-    <td>Philip Cramer</td>
-    <td>Germany</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>North/South</td>
-    <td>Simon Crowther</td>
-    <td>UK</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
-  <tr>
-    <td>Paris spécialités</td>
-    <td>Marie Bertrand</td>
-    <td>France</td>
-    <td style={{display:'flex' ,justifyContent:'space-evenly' }}><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{eye}</button><button style={{ background:'none', border:'none',  marginRight:'10px'}}>{pen}</button><button style={{ background:'none', border:'none'}}>{remove}</button></td>
-  </tr>
+  {todos.map((item, i) => {
+        return <tr key={i} >
+            <td>{item.userId}</td>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.completed === true?'yes':'no'}</td>
+            
+        </tr>;
+        })
+    }
 </table>
 
     </div>
--- src/views/components/Table.tsx
+++ src/views/components/Table.tsx
@@ -1,9 +1,9 @@
 import React ,{ useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getPendingSelector,
+
   getTodosSelector,
-  getErrorSelector,
+
 } from "../../redux/selector";
 import { fetchTodoRequest } from "../../redux/actions";
 
@@ -15,9 +15,8 @@ export const Table = () => {
 
 
   const dispatch = useDispatch();
-  const pending = useSelector(getPendingSelector);
   const todos = useSelector(getTodosSelector);
-  const error = useSelector(getErrorSelector);
+  
 
   useEffect(() => {
     dispatch(fetchTodoRequest());
@@ -29,30 +28,24 @@ export const Table = () => {
  
 
  <button>Completed</button>
-<table id="customers">
+ <table id="customers">
   <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
+    <th>Userid</th>
+    <th>Id</th>
+    <th>Task</th>
     <th>Status</th>
     
   </tr>
   {todos.map((item, i) => {
         return <tr key={i} >
-            <td>{item.title}</td>
-            <td>{item.title}</td>
+            <td>{item.userId}</td>
+            <td>{item.id}</td>
             <td>{item.title}</td>
             <td>{item.completed === true?'yes':'no'}</td>
             
         </tr>;
         })
     }
-  <tr>
-    <td>Paris spécialités</td>
-    <td>Marie Bertrand</td>
-    <td>France</td>
-    <td><img src={inComplete} alt=""  style={{width:'20px' , height:'20px'}}/></td>
-  </tr>
 </table>
 
     </div>
